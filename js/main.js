c310ec01d1ebcf33e56365ebc3b00816d37b4349
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2021-04-02 20:32:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,129 @@
+$(document).ready(function () {
+
+
+    // animated redial progress bar js
+
+    $('svg.radial-progress').each(function (index, value) {
+        $(this).find($('circle.complete')).removeAttr('style');
+    });
+
+    $(window).scroll(function () {
+        $('svg.radial-progress').each(function (index, value) {
+            // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+            if (
+                $(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+                $(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+            ) {
+                // Get percentage of progress
+                percent = $(value).data('percentage');
+                // Get radius of the svg's circle.complete
+                radius = $(this).find($('circle.complete')).attr('r');
+                // Get circumference (2πr)
+                circumference = 2 * Math.PI * radius;
+                // Get stroke-dashoffset value based on the percentage of the circumference
+                strokeDashOffset = circumference - ((percent * circumference) / 100);
+                // Transition progress for 1.25 seconds
+                $(this).find($('circle.complete')).animate({
+                    'stroke-dashoffset': strokeDashOffset
+                }, 1250);
+            }
+        });
+    }).trigger('scroll');
+
+    // sticky top js
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".sticky_top").addClass("menu_bg");
+        } else {
+            $(".sticky_top").removeClass("menu_bg");
+        }
+    });
+
+
+    // Smooths scrolling
+    $('body').scrollspy({
+        target: ".navbar",
+        offset: 50
+    });
+
+    //animation scroll js
+    var html_body = $('html, body');
+    $('nav a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 45
+                }, 1500);
+                return false;
+            }
+        }
+    });
+
+
+    //animation scroll js
+    var html_body = $('html, body');
+    $('.down_button a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 45
+                }, 1500);
+                return false;
+            }
+        }
+    });
+
+    // wow js
+
+    new WOW().init();
+
+    // scroll to top js
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".scroll_to_top").fadeIn();
+        } else {
+            $(".scroll_to_top").fadeOut();
+        }
+    });
+
+    $(".scroll_to_top").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    
+    // preloader js
+    
+    $(window).on("load", function(){
+        $(".preloader").delay(2000).fadeOut(500);
+    });
+    
+    // responsive menu js
+    
+    $(".openNav").click(function(){
+        $(".overlay").css("transform", "scaleY(1)");
+    });
+    
+    $(".closeNav").click(function(){
+        $(".overlay").css("transform", "scaleY(0)");
+    });
+    
+    $(".overlay_top a img").click(function(){
+        $(".overlay").css("transform", "scaleY(0)");
+    });
+    
+    $(".overlay_content ul li a").click(function(){
+        $(".overlay").css("transform", "scaleY(0)");
+    });
+
+
+
+
+
+});
